b02afb3a4f1da76f09a1a01291150dfa15f9848e
secret n <secretn@school.local>
2024-06-13 07:19:55 -0700

    blog .v1

---
+++ app/Models/Blog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'main_image',
+        'content',
+        'long_description',
+        'more_images',
+    ];
+}
